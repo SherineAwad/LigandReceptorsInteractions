88fb336230ac8084de63b5fb92f9ac1e917a35aa
Sherine Awad <sherinem@gl3209.arc-ts.umich.edu>
2024-08-27 11:04:51 -0400

    Update liana.R

--- liana.R
+++ liana.R
@@ -8,16 +8,22 @@ show_methods()
 
 liana_path <- system.file(package = "liana")
 
-mydata <- readRDS("merged3samples.rds")
+args <- commandArgs(trailingOnly = TRUE)
+mysamples <- args[1]
+
+myRDS <- paste(mysample, ".rds", sep="")
+myRDS
+
+mydata <- readRDS(myRDS)
 
 mydata %>% dplyr::glimpse() 
-#liana_test <- liana_wrap(mydata)
+#Could use assay =RNA 
 liana_test <- liana_wrap(mydata, assay='SCT')
 liana_test %>% dplyr::glimpse()
 liana_test <- liana_test %>% liana_aggregate()
 dplyr::glimpse(liana_test)
 
-figure_name <- "merged3samples"
+figure_name <- mysamples
 figure_name <- paste(figure_name,"LR.pdf", sep="")
 pdf(file =figure_name, width=12)
 liana_test %>%
@@ -26,13 +32,14 @@ liana_test %>%
                 ntop = 20)
 
 dev.off() 
-figure_name <- "merged3samples"
+
+
+figure_name <- mysamples
 figure_name <- paste(figure_name,"LRHeatmap.pdf", sep="")
 pdf(file =figure_name, width=12)
-liana_trunc <- liana_test %>%
-   # only keep interactions concordant between methods
-  filter(aggregate_rank <= 0.01) # note that these pvals are already corrected
-
+#only keep interactions concordant between methods
+#note that these pvals are already corrected
+liana_trunc <- liana_test %>% filter(aggregate_rank <= 0.01)
 heat_freq(liana_trunc)
 dev.off() 
 
