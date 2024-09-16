65bd5bdf7254379bebb05ab24aa3efc7f21dad40
Sherine Awad <sherinem@gl-login2.arc-ts.umich.edu>
2024-09-16 18:25:56 -0400

    remove extra lines in Ligan-Receptor.R

--- Ligand-Receptor.R
+++ Ligand-Receptor.R
@@ -30,53 +30,30 @@ zebra_human <- homologene_download(
 zebra_human <- rename(zebra_human,  source_genesymbol =genesymbol_source, target_genesymbol=genesymbol_target )
 head(zebra_human)
 
-
 mydata %>% dplyr::glimpse() 
-#Could use assay =RNA 
+#Could use assay =RNA
 liana_test <- liana_wrap(mydata, assay='RNA', resource ='custom', external_resource=zebra_human)
 liana_test %>% dplyr::glimpse()
-liana_test <- liana_test %>% liana_aggregate(resource=resource)
+liana_test <- liana_test %>% liana_aggregate()
 dplyr::glimpse(liana_test)
 
-figure_name <- mysamples
-figure_name <- paste(figure_name,"LR.pdf", sep="")
+figure_name <- mysample
+figure_name <- paste(figure_name,"LR1.pdf", sep="")
 pdf(file =figure_name, height=20, width=40)
 liana_test %>%
-  liana_dotplot(source_groups = c("Act_MG"),
-                target_groups = c("AC", "RGC", "PreAC", "BC", "Act_MG",),
-                ntop = 30)
+  liana_dotplot(source_groups = c("MG"),
+                target_groups = c("PreAC", "AC", "PreBC", "BC","Pre Rod", "Rod", "MG"), 
+                ntop = 10)
 
 dev.off() 
 
-RGC    BC     clstn2         CLSTN2               0.00000126      104.
- 2 AC     Act_MG cpne8          CPNE8                0.0000143       107.
- 3 Act_MG AC     cacna2d1a      CACNA2D1             0.0000196       104.
- 4 RGC    RGC    nrxn2a         NRXN2                0.0000657       142.
- 5 AC     Stage1 cpne8          CPNE8                0.0000954       121.
- 6 PreAC  Act_MG cpne8          CPNE8                0.000133        119.
- 7 BC     BC     clstn2         CLSTN2               0.000155        138.
- 8 RGC    MG     adam12         ADAM12               0.000179        144.
- 9 PreAC  AC     cacna2d3       CACNA2D3             0.000206        165.
-10 Act_MG PreAC
-
-
-
-figure_name <- mysamples
-figure_name <- paste(figure_name,"LRHeatmap.pdf", sep="")
-pdf(file =figure_name, width=12)
-#only keep interactions concordant between methods
-#note that these pvals are already corrected
-liana_trunc <- liana_test %>% filter(aggregate_rank <= 0.01)
-heat_freq(liana_trunc)
-dev.off() 
-
-
-figure_name <- mysamples
-figure_name <- paste(figure_name,"Chord.pdf", sep="")
-pdf(file =figure_name, width=12)
-p <- chord_freq(liana_trunc,
-                source_groups = c(""Mullerglia"),
-                target_groups = c("Rod", "Cone", "Mullerglia"))
+figure_name <- mysample
+figure_name <- paste(figure_name,"LR2.pdf", sep="")
+pdf(file =figure_name, height=20, width=40)
+liana_test %>%
+  liana_dotplot(source_groups = c("MG"),
+                target_groups = c("RGC", "Pre Cone" ,"Cone", "Stage1","Stage2" , "Act_MG","MG"),
+                ntop = 10)
 
 dev.off() 
 
