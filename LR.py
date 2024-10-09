989ae8d26b60ab88bad1de15f7c7cb5a2aa66f41
Sherine Awad <sherinem@gl-login3.arc-ts.umich.edu>
2024-10-09 13:29:05 -0400

    Update LR.py using liana++ and detch R scripts from liana

--- LR.py
+++ LR.py
@@ -8,18 +8,14 @@ import omnipath as op
 import decoupler as dc
 import pandas as pd
 from liana.method import singlecellsignalr, connectome, cellphonedb, natmi, logfc, cellchat, geometric_mean
+from liana.mt import rank_aggregate
+from liana.method import singlecellsignalr, connectome, cellphonedb, natmi, logfc, cellchat, geometric_mean
 
 
 def toZebra():
      resource = li.rs.select_resource('consensus')
      resource.head()
-     map_df = li.rs.get_hcop_orthologs(url='https://ftp.ebi.ac.uk/pub/databases/genenames/hcop/human_zebrafish_hcop_fifteen_column.txt.gz',
-                                   columns=['human_symbol', 'zebrafish_ensembl_gene'],
-                                   # NOTE: HCOP integrates multiple resource, so we can filter out mappings in at least 3 of them for confidence
-                                   min_evidence=3
-                                   )
-
-     #zebrafish_symbol
+     #we can use other nameID as in [human_entrez_gene	human_ensembl_gene	hgnc_id	human_name	human_symbol	human_chr	human_assert_ids	zebrafish_entrez_gene	zebrafish_ensembl_gene	zfin_id	zebrafish_name	zebrafish_symbol	zebrafish_chr	zebrafish_assert_ids]
      map_df = li.rs.get_hcop_orthologs(url='https://ftp.ebi.ac.uk/pub/databases/genenames/hcop/human_zebrafish_hcop_fifteen_column.txt.gz',
                                    columns=['human_symbol', 'zebrafish_symbol'],
                                    # NOTE: HCOP integrates multiple resource, so we can filter out mappings in at least 3 of them for confidence
@@ -27,7 +23,6 @@ def toZebra():
                                    )
 
      # rename the columns to source and target, respectively for the original organism and the target organism
-     map_df = map_df.rename(columns={'human_symbol':'source', 'zebrafish_ensembl_gene':'target'})
      map_df = map_df.rename(columns={'human_symbol':'source', 'zebrafish_symbol':'target'})
      map_df.tail()
 
@@ -48,7 +43,7 @@ def main():
          zfish.to_csv('zebraEns_LR.csv', sep='\t', index=False, encoding='utf-8')
      
      adata = sc.read_h5ad("Injury_snRNA_allClusters.h5ad")
-     for line in open("mappingEnsmbl.csv"):
+     for line in open("mapping.csv"): #if we use Ensembl ID we need mappignEnsmbl.csv 
           if "old" in line: 
              continue
           records = (line.strip()).split(",")
@@ -56,12 +51,10 @@ def main():
      #adata.raw.X
      print(adata.var_names)
      print(adata.var)
-     #li.mt.show_methods()
+     li.mt.show_methods()
 
-     from liana.mt import rank_aggregate
-
-     from liana.method import singlecellsignalr, connectome, cellphonedb, natmi, logfc, cellchat, geometric_mean
-     #{'AC', 'MGPCs', 'Rod_p', 'BC', 'RestMG', 'RGC_p', 'RGC', 'Rod', 'AC_p', 'HC', 'ActivateMG', 'BC_p', 'Cone_p', 'Microglia', 'Cone'}
+     #new_cellype we have are {'AC', 'MGPCs', 'Rod_p', 'BC', 'RestMG', 'RGC_p', 'RGC', 'Rod', 'AC_p', 'HC', 'ActivateMG', 'BC_p', 'Cone_p', 'Microglia', 'Cone'}
+     #we use inplace=False to print to file 
      phonePD = cellphonedb(adata,groupby='new_celltype',resource_name='consensus',resource=zfish,expr_prop=0, key_added='cpdb_res', use_raw = False,inplace = False)
      phonePD.to_csv("cellphonedbLR.csv", sep='\t', encoding='utf-8', index=False, header=True) 
      cellchatPD = cellchat(adata,groupby='new_celltype',resource_name='consensus',resource=zfish,expr_prop=0, key_added='cchat_res',use_raw = False, inplace = False)
@@ -70,6 +63,7 @@ def main():
      scPD.to_csv("singlecelldbLR.csv", sep='\t', encoding='utf-8', index=False, header=True)
      
      
+     #we run inplace =True for plots     
      cellphonedb(adata,groupby='new_celltype',resource_name='consensus',resource=zfish,expr_prop=0, key_added='cpdb_res', use_raw = False)
      p1 = li.pl.dotplot(adata = adata,
               colour='lr_means',
@@ -94,7 +88,7 @@ def main():
               inverse_size=True, # we inverse sign since we want small p-values to have large sizes
               source_labels=['Microglia'],
               target_labels=['MGPCs','Rod', 'Cone', 'RGC','ActivateMG'],
-              figure_size=(20, 10),
+              figure_size=(25, 10),
               filter_fun=lambda x: x['cellchat_pvals'] <= 0.001,
               top_n=10,
               orderby='cellchat_pvals',
@@ -111,7 +105,7 @@ def main():
               inverse_size=True, # we inverse sign since we want small p-values to have large sizes
               source_labels=['Microglia'],
               target_labels=['MGPCs','Rod', 'Cone', 'RGC','ActivateMG'],
-              figure_size=(20, 10),
+              figure_size=(25, 10),
               top_n=10,
               orderby='lrscore',
               orderby_ascending=True,
@@ -141,21 +135,6 @@ def main():
              )
      p4.save("aggregate.png")
 
-     p5 = li.pl.tileplot(adata = adata,
-                         # NOTE: fill & label need to exist for both
-                         # ligand_ and receptor_ columns
-                         fill='lr_means',
-                         label='lr_means',
-                         top_n=10,
-                         orderby='specificity_rank',
-                         orderby_ascending=True,
-                         source_labels=['ActivateMG'],
-                         target_labels=['MGPCs','Rod', 'Cone', 'RGC','ActivateMG'],
-                         source_title='Ligand',
-                         target_title='Receptor',
-                         figure_size=(25, 15)
-                         )
-      p5.save("aggregateTile.png")
 if __name__ == '__main__':
     main()
 
